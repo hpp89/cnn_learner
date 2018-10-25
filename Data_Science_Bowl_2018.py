6547aa1e411a3bf161650f7902d6415728bfe741
hpp89 <731669897@qq.com>
2018-10-25 18:52:23 +0800

    1

--- Data_Science_Bowl_2018.py
+++ Data_Science_Bowl_2018.py
@@ -18,13 +18,15 @@ from keras.layers import Dropout, Conv2D, MaxPooling2D, Lambda, Conv2DTranspose,
 from keras.layers import concatenate
 from keras.callbacks import EarlyStopping, ModelCheckpoint
 from keras.initializers import he_normal
+from keras.regularizers import l2
 from keras import backend as K
 
 import tensorflow as tf
 
 seed = 42
-random.seed = seed
-np.random.seed(seed)
+weight_decay = 0.001
+# random.seed = seed
+# np.random.seed(seed)
 warnings.filterwarnings('ignore', category=UserWarning, module='skimage')
 
 IMAGE_SIZE = 128
@@ -32,29 +34,27 @@ CHANNELS = 3
 DROPOUT = [0.1, 0.2, 0.3]
 
 TRAIN_PATH = './stage1_train/'
-TEST_PATH = './stage2_test_final/'
+TEST_PATH = './stage1_test/'
 
 train_ids = next(os.walk(TRAIN_PATH))[1]
 test_ids = next(os.walk(TEST_PATH))[1]
-
-print(train_ids)
 
 X_train = np.zeros((len(train_ids), IMAGE_SIZE, IMAGE_SIZE, CHANNELS), dtype=np.uint8)
 Y_train = np.zeros((len(train_ids), IMAGE_SIZE, IMAGE_SIZE, 1), dtype=np.bool)
 
 sys.stdout.flush()
-# for n, id_ in tqdm(enumerate(train_ids), total=len(train_ids)):
-#     path = TRAIN_PATH + id_
-#     img = imread(path + '/images/' + id_ + '.png')[:, :, :CHANNELS]
-#     img = resize(img, (IMAGE_SIZE, IMAGE_SIZE), mode='constant', preserve_range=True)
-#     X_train[n] = img
-#     mask = np.zeros((IMAGE_SIZE, IMAGE_SIZE, 1), dtype=np.bool)
-#     for mask_file in next(os.walk(path + '/masks/'))[2]:
-#         mask_ = imread(path + '/masks/' + mask_file)
-#         mask_ = np.expand_dims(resize(mask_, (IMAGE_SIZE, IMAGE_SIZE), mode='constant',
-#                                       preserve_range=True), axis=-1)
-#         mask = np.maximum(mask, mask_)
-#     Y_train[n] = mask
+for n, id_ in tqdm(enumerate(train_ids), total=len(train_ids)):
+    path = TRAIN_PATH + id_
+    img = imread(path + '/images/' + id_ + '.png')[:, :, :CHANNELS]
+    img = resize(img, (IMAGE_SIZE, IMAGE_SIZE), mode='constant', preserve_range=True)
+    X_train[n] = img
+    mask = np.zeros((IMAGE_SIZE, IMAGE_SIZE, 1), dtype=np.bool)
+    for mask_file in next(os.walk(path + '/masks/'))[2]:
+        mask_ = imread(path + '/masks/' + mask_file)
+        mask_ = np.expand_dims(resize(mask_, (IMAGE_SIZE, IMAGE_SIZE), mode='constant',
+                                      preserve_range=True), axis=-1)
+        mask = np.maximum(mask, mask_)
+    Y_train[n] = mask
 
 # Get and resize test images
 X_test = np.zeros((len(test_ids), IMAGE_SIZE, IMAGE_SIZE, CHANNELS), dtype=np.uint8)
@@ -86,53 +86,53 @@ def mean_iou(y_true, y_pred):
 # inputs = Input((IMAGE_SIZE, IMAGE_SIZE, CHANNELS))
 # s = Lambda(lambda x: x / 255.0)(inputs)
 #
-# c1 = Conv2D(16, (3, 3), activation='elu', kernel_initializer=he_normal(), padding='same')(s)
+# c1 = Conv2D(16, (3, 3), activation='elu', kernel_initializer=he_normal(), kernel_regularizer=l2(weight_decay), padding='same')(s)
 # c1 = Dropout(DROPOUT[0])(c1)
-# c1 = Conv2D(16, (3, 3), activation='elu', kernel_initializer=he_normal(), padding='same')(c1)
+# c1 = Conv2D(16, (3, 3), activation='elu', kernel_initializer=he_normal(), kernel_regularizer=l2(weight_decay), padding='same')(c1)
 # p1 = MaxPooling2D((2, 2))(c1)
 #
-# c2 = Conv2D(32, (3, 3), activation='elu', kernel_initializer=he_normal(), padding='same')(p1)
+# c2 = Conv2D(32, (3, 3), activation='elu', kernel_initializer=he_normal(), kernel_regularizer=l2(weight_decay), padding='same')(p1)
 # c2 = Dropout(DROPOUT[0])(c2)
-# c2 = Conv2D(32, (3, 3), activation='elu', kernel_initializer=he_normal(), padding='same')(c2)
+# c2 = Conv2D(32, (3, 3), activation='elu', kernel_initializer=he_normal(), kernel_regularizer=l2(weight_decay), padding='same')(c2)
 # p2 = MaxPooling2D((2, 2))(c2)
 #
-# c3 = Conv2D(64, (3, 3), activation='elu', kernel_initializer=he_normal(), padding='same')(p2)
+# c3 = Conv2D(64, (3, 3), activation='elu', kernel_initializer=he_normal(), kernel_regularizer=l2(weight_decay), padding='same')(p2)
 # c3 = Dropout(DROPOUT[1])(c3)
-# c3 = Conv2D(64, (3, 3), activation='elu', kernel_initializer=he_normal(), padding='same')(c3)
+# c3 = Conv2D(64, (3, 3), activation='elu', kernel_initializer=he_normal(), kernel_regularizer=l2(weight_decay), padding='same')(c3)
 # p3 = MaxPooling2D((2, 2))(c3)
 #
-# c4 = Conv2D(128, (3, 3), activation='elu', kernel_initializer=he_normal(), padding='same')(p3)
+# c4 = Conv2D(128, (3, 3), activation='elu', kernel_initializer=he_normal(), kernel_regularizer=l2(weight_decay), padding='same')(p3)
 # c4 = Dropout(DROPOUT[1])(c4)
-# c4 = Conv2D(128, (3, 3), activation='elu', kernel_initializer=he_normal(), padding='same')(c4)
+# c4 = Conv2D(128, (3, 3), activation='elu', kernel_initializer=he_normal(), kernel_regularizer=l2(weight_decay), padding='same')(c4)
 # p4 = MaxPooling2D((2, 2))(c4)
 #
-# c5 = Conv2D(256, (3, 3), activation='elu', kernel_initializer=he_normal(), padding='same')(p4)
+# c5 = Conv2D(256, (3, 3), activation='elu', kernel_initializer=he_normal(), kernel_regularizer=l2(weight_decay), padding='same')(p4)
 # c5 = Dropout(DROPOUT[2])(c5)
-# c5 = Conv2D(256, (3, 3), activation='elu', kernel_initializer=he_normal(), padding='same')(c5)
+# c5 = Conv2D(256, (3, 3), activation='elu', kernel_initializer=he_normal(), kernel_regularizer=l2(weight_decay), padding='same')(c5)
 #
 # u6 = Conv2DTranspose(128, (2, 2), strides=(2, 2), padding='same')(c5)
 # u6 = concatenate([u6, c4])
-# c6 = Conv2D(128, (3, 3), activation='elu', kernel_initializer=he_normal(), padding='same')(u6)
+# c6 = Conv2D(128, (3, 3), activation='elu', kernel_initializer=he_normal(), kernel_regularizer=l2(weight_decay), padding='same')(u6)
 # c6 = Dropout(DROPOUT[1])(c6)
-# c6 = Conv2D(128, (3, 3), activation='elu', kernel_initializer=he_normal(), padding='same')(c6)
+# c6 = Conv2D(128, (3, 3), activation='elu', kernel_initializer=he_normal(), kernel_regularizer=l2(weight_decay), padding='same')(c6)
 #
 # u7 = Conv2DTranspose(64, (2, 2), strides=(2, 2), padding='same')(c6)
 # u7 = concatenate([u7, c3])
-# c7 = Conv2D(64, (3, 3), activation='elu', kernel_initializer=he_normal(), padding='same')(u7)
+# c7 = Conv2D(64, (3, 3), activation='elu', kernel_initializer=he_normal(), kernel_regularizer=l2(weight_decay), padding='same')(u7)
 # c7 = Dropout(DROPOUT[1])(c7)
-# c7 = Conv2D(64, (3, 3), activation='elu', kernel_initializer=he_normal(), padding='same')(c7)
+# c7 = Conv2D(64, (3, 3), activation='elu', kernel_initializer=he_normal(), kernel_regularizer=l2(weight_decay), padding='same')(c7)
 #
 # u8 = Conv2DTranspose(32, (2, 2), strides=(2, 2), padding='same')(c7)
 # u8 = concatenate([u8, c2])
-# c8 = Conv2D(32, (3, 3), activation='elu', kernel_initializer=he_normal(), padding='same')(u8)
+# c8 = Conv2D(32, (3, 3), activation='elu', kernel_initializer=he_normal(), kernel_regularizer=l2(weight_decay), padding='same')(u8)
 # c8 = Dropout(DROPOUT[0])(c8)
-# c8 = Conv2D(32, (3, 3), activation='elu', kernel_initializer=he_normal(), padding='same')(c8)
+# c8 = Conv2D(32, (3, 3), activation='elu', kernel_initializer=he_normal(), kernel_regularizer=l2(weight_decay), padding='same')(c8)
 #
 # u9 = Conv2DTranspose(16, (2, 2), strides=(2, 2), padding='same')(c8)
 # u9 = concatenate([u9, c1])
-# c9 = Conv2D(16, (3, 3), activation='elu', kernel_initializer=he_normal(), padding='same')(u9)
+# c9 = Conv2D(16, (3, 3), activation='elu', kernel_initializer=he_normal(), kernel_regularizer=l2(weight_decay), padding='same')(u9)
 # c9 = Dropout(DROPOUT[0])(c9)
-# c9 = Conv2D(16, (3, 3), activation='elu', kernel_initializer=he_normal(), padding='same')(c9)
+# c9 = Conv2D(16, (3, 3), activation='elu', kernel_initializer=he_normal(), kernel_regularizer=l2(weight_decay), padding='same')(c9)
 #
 # outputs = Conv2D(1, (1, 1), activation='sigmoid')(c9)
 #
@@ -146,13 +146,13 @@ def mean_iou(y_true, y_pred):
 #                     callbacks=[earlystopper, checkpointer])
 
 model = load_model('model-dsbowl2018-1.h5', custom_objects={'mean_iou': mean_iou})
-# preds_train = model.predict(X_train[:int(X_train.shape[0] * 0.9)], verbose=1)
-# preds_val = model.predict(X_train[int(X_train.shape[0] * 0.9):], verbose=1)
+preds_train = model.predict(X_train[:int(X_train.shape[0] * 0.9)], verbose=1)
+preds_val = model.predict(X_train[int(X_train.shape[0] * 0.9):], verbose=1)
 preds_test = model.predict(X_test, verbose=1)
 
 # Threshold predictions
-# preds_train_t = (preds_train > 0.5).astype(np.uint8)
-# preds_val_t = (preds_val > 0.5).astype(np.uint8)
+preds_train_t = (preds_train > 0.5).astype(np.uint8)
+preds_val_t = (preds_val > 0.5).astype(np.uint8)
 preds_test_t = (preds_test > 0.5).astype(np.uint8)
 
 # Create list of upsampled test masks
@@ -162,13 +162,13 @@ for i in range(len(preds_test)):
                                        (sizes_test[i][0], sizes_test[i][1]),
                                        mode='constant', preserve_range=True))
 
-# ix = random.randint(0, len(preds_train_t))
-# imshow(X_train[ix])
-# plt.show()
-# imshow(np.squeeze(Y_train[ix]))
-# plt.show()
-# imshow(np.squeeze(preds_train_t[ix]))
-# plt.show()
+ix = random.randint(0, len(preds_train_t))
+imshow(X_train[ix])
+plt.show()
+imshow(np.squeeze(Y_train[ix]))
+plt.show()
+imshow(np.squeeze(preds_train_t[ix]))
+plt.show()
 
 
 def rle_encoding(x):
@@ -198,4 +198,4 @@ for n, id_ in enumerate(test_ids):
 sub = pd.DataFrame()
 sub['ImageId'] = new_test_ids
 sub['EncodedPixels'] = pd.Series(rles).apply(lambda x: ' '.join(str(y) for y in x))
-sub.to_csv('sub-dsbowl2018-2.csv', index=False)
+sub.to_csv('sub-dsbowl2018-1.csv', index=False)
---
+++ seedlings_classify.py
@@ -0,0 +1,151 @@
+import os
+import cv2
+
+import numpy as np
+import pandas as pd
+
+import keras
+from keras.models import Model, load_model, Input
+from keras.layers import MaxPooling2D, Conv2D, Dense, Activation
+from keras.layers import Flatten, Softmax, BatchNormalization, Dropout
+from keras.initializers import he_normal
+from keras.regularizers import l2
+from keras.callbacks import EarlyStopping, ModelCheckpoint
+from tqdm import tqdm
+
+CLASS = {
+    'Black-grass': 0,
+    'Charlock': 1,
+    'Cleavers': 2,
+    'Common Chickweed': 3,
+    'Common wheat': 4,
+    'Fat Hen': 5,
+    'Loose Silky-bent': 6,
+    'Maize': 7,
+    'Scentless Mayweed': 8,
+    'Shepherds Purse': 9,
+    'Small-flowered Cranesbill': 10,
+    'Sugar beet': 11
+}
+
+train_path = './seedlings/train'
+test_path = './seedlings/test'
+
+WEIGHT_DELAY = 0.001
+IMAGE_SIZE = 128
+IMAGE_CHANNELS = 3
+NUM_CLASSES = 12
+CHECK_POINT_PATH = 'model-seedlings.h5'
+
+X_train = np.ndarray([4750, IMAGE_SIZE, IMAGE_SIZE, IMAGE_CHANNELS])
+Y_train = []
+
+
+def get_train_data():
+    global Y_train
+    global X_train
+
+    count = 0
+    for name_dir in os.listdir(train_path):
+        sub_name_list = os.listdir('{}/{}'.format(train_path, name_dir))
+        Y_train.extend([CLASS[name_dir]] * len(sub_name_list))
+        for sub_name in tqdm(sub_name_list, total=len(sub_name_list)):
+            img = cv2.imread('{}/{}/{}'.format(train_path, name_dir, sub_name))
+            img = cv2.resize(img, (IMAGE_SIZE, IMAGE_SIZE))
+            img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+            X_train[count, :, :, :] = img
+            count += 1
+
+    Y_train = keras.utils.to_categorical(Y_train, NUM_CLASSES)
+    X_train = X_train / 255.0
+
+
+X_test = np.ndarray([4750, IMAGE_SIZE, IMAGE_SIZE, IMAGE_CHANNELS])
+
+
+def get_test_data():
+    global X_test
+
+    count = 0
+    for name_dir in os.listdir(test_path):
+        img = cv2.imread('{}/{}'.format(test_path, name_dir))
+        img = cv2.resize(img, (IMAGE_SIZE, IMAGE_SIZE))
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        X_test[count, :, :, :] = img
+        count += 1
+
+    X_test = X_test / 255.0
+
+
+def conv2_block(in_data, filters):
+    conv = Conv2D(filters, kernel_size=(3, 3), kernel_initializer=he_normal(), kernel_regularizer=l2(WEIGHT_DELAY), padding='same')(in_data)
+    bn = BatchNormalization()(conv)
+    out_data = Activation('relu')(bn)
+
+    return out_data
+
+
+def max_pooling_block(in_data):
+    out_data = MaxPooling2D((2, 2), padding='same')(in_data)
+
+    return out_data
+
+
+def dense_block(in_data, filters, activation='relu'):
+    dense = Dense(filters, kernel_initializer=he_normal(), kernel_regularizer=l2(WEIGHT_DELAY))(in_data)
+    bn = BatchNormalization()(dense)
+    out_data = Activation(activation=activation)(bn)
+
+    return out_data
+
+
+def get_model():
+    inputs = Input(shape=(IMAGE_SIZE, IMAGE_SIZE, IMAGE_CHANNELS))
+    c1 = conv2_block(inputs, 64)
+    c1 = conv2_block(c1, 64)
+    p1 = max_pooling_block(c1)
+
+    c2 = conv2_block(p1, 128)
+    c2 = conv2_block(c2, 128)
+    p2 = max_pooling_block(c2)
+
+    c3 = conv2_block(p2, 256)
+    c3 = conv2_block(c3, 256)
+    c3 = conv2_block(c3, 256)
+    p3 = max_pooling_block(c3)
+
+    flat = Flatten()(p3)
+    d1 = dense_block(flat, 128)
+    d2 = dense_block(d1, 12, activation='softmax')
+
+    model = Model(inputs=inputs, outputs=d2)
+    model.compile('adam', loss='categorical_crossentropy', metrics=['accuracy'])
+    model.summary()
+
+    return model
+
+
+def train():
+    get_train_data()
+
+    model = get_model()
+    earlystopper = EarlyStopping(patience=0.5, verbose=1)
+    checkpointer = ModelCheckpoint(filepath=CHECK_POINT_PATH, verbose=1, save_best_only=True)
+
+    model.fit(X_train, Y_train, validation_split=0.1, batch_size=16, epochs=50,
+              callbacks=[earlystopper, checkpointer])
+
+
+def test():
+    get_test_data()
+
+    model = load_model(CHECK_POINT_PATH)
+    Y_test = model.predict(X_test, verbose=1)
+
+    Y_test = np.argmax(Y_test, axis=1)
+
+
+
+
+if __name__ == '__main__':
+    train()
